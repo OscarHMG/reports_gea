43646aed358437c995400236b4ba8c8e5e98e660
OscarHMG <oscar.moncayo@interlancompu.com>
2017-09-15 14:28:23 -0500

    Init commit

---
+++ view/test.tpl.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<title>TEST</title>
+	</head>
+	<body>
+		<div>HELLO WORLD</div>	
+	</body>
+</html>
